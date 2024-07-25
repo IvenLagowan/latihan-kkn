dd116d70ee1a42d6c53ab62b9774bfed36fa7a53
Iven Lagowan <94290469+IvenLagowan@users.noreply.github.com>
2024-07-25 23:35:43 +0900

    Create update.php

---
+++ update.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Update data
+$sql = "UPDATE mytable SET name='Jane Doe' WHERE id=1";
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
