aa50dbe875af0ba538ab164da1399cb40d2af865
Iven Lagowan <94290469+IvenLagowan@users.noreply.github.com>
2024-07-25 23:36:13 +0900

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Delete data
+$sql = "DELETE FROM mytable WHERE id=1";
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
